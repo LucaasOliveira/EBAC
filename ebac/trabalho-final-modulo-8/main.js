6b4a46d62fe302eaaaa77efbcecbf7b35f56fa9c
Lucas Almeida <oalmeidaluccas@gmail.com>
2024-05-15 00:19:32 -0300

    feat: comit inicial

---
+++ ebac/trabalho-final-modulo-8/main.js
@@ -0,0 +1,39 @@
+const listaContato = [];
+let linha;
+
+const form = document.querySelector("#form-contatos");
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  cadastrarContato();
+  atualizaLista();
+
+});
+
+
+function cadastrarContato() {
+  const inputNomeContato = document.querySelector("#nome-contato");
+  const inputTelefoneContato = document.querySelector("#telefone-contato");
+  
+  if (listaContato.includes(inputNomeContato.value)){
+    alert(`Contato ${inputNomeContato.value} já estava cadastrado.`);
+    form.reset();
+  } else {
+    listaContato.push(inputNomeContato.value);
+    listaContato.push(inputTelefoneContato.value);
+    
+    linha = document.createElement("tr");
+    const colNome = document.createElement("td");
+    colNome.textContent = inputNomeContato.value;
+    const colTelefone = document.createElement("td");
+    colTelefone.textContent = inputTelefoneContato.value;
+    
+    linha.appendChild(colNome);
+    linha.appendChild(colTelefone);
+  };
+};
+
+function atualizaLista() {
+  const ConteudoTabela = document.querySelector("#linha-contato");
+  ConteudoTabela.appendChild(linha)
+};
